8f930245f692a0db18b268ca48deef55e89c87e5
Sophie Åkesson <sophie.akesson@osynlig.se>
2024-01-24 12:47:38 +0100

    Rename lib to utils

--- app/lib/transformCities.ts
+++
@@ -1,8 +0,0 @@
-import { DropdownOption } from '@/components/Dropdown/Dropdown';
-import { City } from '@/types/city';
-
-export const transformCities = (cities: City[]): DropdownOption[] => {
-  return cities.map((city) => {
-    return { name: city.name, value: city.name };
-  });
-};
