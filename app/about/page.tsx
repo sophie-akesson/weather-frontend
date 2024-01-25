628f76b4b7f235eb259e6f23e04a9156fd0032fe
Sophie Åkesson <sophie.akesson@osynlig.se>
2024-01-25 23:44:18 +0100

    Update about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,13 @@
+import styles from './page.module.css';
+
 export default function About() {
   return (
     <main>
-      <p>Denna sida är byggd med data från SMHI.</p>
+      <div className={styles.about}>
+        <p>Denna sida är byggd med data från SMHI.</p>
+        <p>För dagens prognos visas högsta temperaturen under dygnets gång.</p>
+        <p>För regn visas det minsta möjliga värdet.</p>
+      </div>
     </main>
   );
 }
